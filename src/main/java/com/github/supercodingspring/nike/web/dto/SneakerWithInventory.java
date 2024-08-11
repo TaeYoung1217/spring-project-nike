08ed4a6eec99d67bc5e4d9950930059ba69b719a
TaeYoung1217 <wkdxodud44@gmail.com>
2024-08-11 20:42:54 +0900

    주문 상세를 출력하기 위한 객체 구현

---
+++ src/main/java/com/github/supercodingspring/nike/web/dto/SneakerWithInventory.java
@@ -0,0 +1,17 @@
+package com.github.supercodingspring.nike.web.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class SneakerWithInventory {
+    private Integer modelId;
+    private List<String> model_traits;
+    private Double price;
+    private List<SneakerInventory> sneaker_inventories;
+}
