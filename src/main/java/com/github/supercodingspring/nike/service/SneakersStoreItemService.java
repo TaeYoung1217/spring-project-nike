5db500654e3c9f6b03b8dc2035907a42ab370aa1
TaeYoung1217 <wkdxodud44@gmail.com>
2024-08-11 20:41:45 +0900

    - id를 받아 model_id, model_traits, price, sneaker_inventories를 출력하는 메소드 구현
    - model_id, user_id, shipping_address, sneaker_size, order_quantity를 받아 Order를 생성하고 DB에 저장 후 total_price를 return 하는 메소드 구현

--- src/main/java/com/github/supercodingspring/nike/service/SneakersStoreItemService.java
+++ src/main/java/com/github/supercodingspring/nike/service/SneakersStoreItemService.java
@@ -1,8 +1,11 @@
 package com.github.supercodingspring.nike.service;
 
-import com.github.supercodingspring.nike.repository.Sneaker;
-import com.github.supercodingspring.nike.repository.SneakersStoreItemJpaRepository;
+import com.github.supercodingspring.nike.repository.*;
+import com.github.supercodingspring.nike.service.exceptions.NotFoundException;
 import com.github.supercodingspring.nike.service.mapper.SneakerMapper;
+import com.github.supercodingspring.nike.web.dto.OrderBody;
+import com.github.supercodingspring.nike.web.dto.SneakerInventory;
+import com.github.supercodingspring.nike.web.dto.SneakerWithInventory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
@@ -11,17 +14,49 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import com.github.supercodingspring.nike.web.dto.SneakerDto;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class SneakersStoreItemService {
-    private final SneakersStoreItemJpaRepository sneakersStoreItemJpaRepository;
+    private final SneakerJpaRepository sneakerJpaRepository;
+    private final OrderJpaRepository orderJpaRepository;
 
-    @Cacheable(value = "sneaker", key = "#root.methodName")
+    //@Cacheable(value = "sneaker", key = "#root.methodName")
     public Page<SneakerDto> findAllItem(Pageable pageable) {
-        Page<Sneaker> sneakers = sneakersStoreItemJpaRepository.findAll(pageable);
+        Page<Sneaker> sneakers = sneakerJpaRepository.findAll(pageable);
         return sneakers.map(SneakerMapper.INSTANCE::sneakerToSneakerDto);
+    }
+
+    public Double findPriceById(String id){
+        Sneaker sneaker = sneakerJpaRepository.findById(Integer.parseInt(id)).orElseThrow(()->new NotFoundException("model ID인 "+ id +"를 찾을 수 없습니다."));
+        return sneaker.getPrice();
+
+    }
+    public SneakerWithInventory findItemById(String id) {
+        Sneaker sneaker = sneakerJpaRepository.
+                findById(Integer.parseInt(id)).orElseThrow(()->new NotFoundException("model ID인 "+ id +"를 찾을 수 없습니다."));
+
+        List<SneakerInventory> sneakerInventoryList = sneaker.getKoreaInventories().stream().map(SneakerInventory::new).toList();
+
+        return new SneakerWithInventory(sneaker.getModelId(),
+                sneaker.getSneakerModelTraits().stream().map(sneakerModelTraits -> sneakerModelTraits.getSneakerTraits().getDescriptions()).toList(),
+                sneaker.getPrice(),
+                sneakerInventoryList);
+    }
+
+    public String orderItem(OrderBody orderBody) {
+        Double totalPrice = findPriceById(String.valueOf(orderBody.getModelId())) * orderBody.getOrderQuantity();
+        Orders orders = new Orders(orderBody);
+        orders.setOrderAt(LocalDateTime.now());
+        orders.setTotalPrice(totalPrice);
+        orders.setOrderStatus(OrderStatus.ORDER_COMPLETED);
+        orderJpaRepository.save(orders);
+
+        return "구매하신 물품 "+totalPrice+"이고 예약완료 되었습니다.";
     }
 }
--- src/main/java/com/github/supercodingspring/nike/web/controller/SneakersStoreController.java
+++ src/main/java/com/github/supercodingspring/nike/web/controller/SneakersStoreController.java
@@ -1,16 +1,17 @@
 package com.github.supercodingspring.nike.web.controller;
 
 import com.github.supercodingspring.nike.service.SneakersStoreItemService;
+import com.github.supercodingspring.nike.web.dto.OrderBody;
+import com.github.supercodingspring.nike.web.dto.SneakerWithInventory;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.github.supercodingspring.nike.web.dto.SneakerDto;
 
 import java.util.List;
@@ -32,6 +33,18 @@ public class SneakersStoreController {
     @GetMapping("/user-items/sneakers")
     public Page<SneakerDto> findAllSneakers(Pageable pageable){
         return sneakersStoreItemService.findAllItem(pageable);
+    }
+
+    @GetMapping("/user-items/sneakers/{id}")
+    public SneakerWithInventory findWithId(@PathVariable String id){
+        return sneakersStoreItemService.findItemById(id);
+    }
+
+    @PostMapping("/user-order-wish/order")
+    public String orderSneakers(
+            @Parameter(description = "model_id, user_id, shipping_address, sneaker_size, order_quantity", required = true)
+            @RequestBody OrderBody orderBody){
+        return sneakersStoreItemService.orderItem(orderBody);
     }
 
 }
