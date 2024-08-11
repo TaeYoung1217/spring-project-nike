671e539d1ed8e5739234390f75e2754f98105174
TaeYoung1217 <wkdxodud44@gmail.com>
2024-08-11 20:35:40 +0900

    전역 예외 처리 추가

---
+++ src/main/java/com/github/supercodingspring/nike/service/exceptions/NotAcceptException.java
@@ -0,0 +1,7 @@
+package com.github.supercodingspring.nike.service.exceptions;
+
+public class NotAcceptException extends RuntimeException {
+    public NotAcceptException(String message) {
+        super(message);
+    }
+}
